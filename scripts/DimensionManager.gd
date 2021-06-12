1fe7dc70a4f89a93f88c20610573a4d7229de42a
Matthew Russell <ernematthew@googlemail.com>
2021-06-12 14:51:52 +0100

    Move HUD to canvas layer

--- scripts/DimensionManager.gd
+++ scripts/DimensionManager.gd
@@ -5,13 +5,21 @@ class_name DimensionManager
 signal end_portal_entered
 
 onready var walls: TileMap = $Walls
+onready var tutorial: CanvasLayer = $Tutorial
+
+var tutorial_texts
 
 const PLAYER_COLLISION_BIT = 1
+
+func _ready() -> void:
+    tutorial_texts = tutorial.get_children()
 
 func set_active(active: bool) -> void:
     visible = active
     walls.set_collision_mask_bit(PLAYER_COLLISION_BIT, active)
     walls.set_occluder_light_mask(1 if active else 0)
+    for tutorial_text in tutorial_texts:
+        tutorial_text.visible = active
 
 func _on_end_portal_entered(body: Node) -> void:
     emit_signal("end_portal_entered", body)
--- ui/HUD.gd
+++ ui/HUD.gd
@@ -2,7 +2,8 @@ extends Control
 
 class_name HUD
 
-onready var energy_bar_container: Control = $EnergyBar
+onready var energy_bar_icon: TextureRect = $EnergyBar/Icon
+onready var energy_bar_background: TextureRect = $EnergyBar/Background
 onready var energy_bar: TextureRect = $EnergyBar/Bar
 
 
@@ -11,4 +12,6 @@ func _on_dark_time_remaining(time: float, max_time: float) -> void:
 
 
 func set_dimension(light: bool) -> void:
-    energy_bar_container.visible = !light
+    energy_bar.visible = !light
+    energy_bar_background.visible = !light
+    energy_bar_icon.visible = !light
